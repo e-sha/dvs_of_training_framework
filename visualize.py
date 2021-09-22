78c9030ec288eadd8bc85de9c31c8ce37a8f0ad0
Eugene Shalnov <eshalnov@google.com>
2021-09-22 22:07:07 +0300

    Add notebook to statistics on a validation set

--- visualize.py
+++ visualize.py
@@ -7,6 +7,7 @@ from PIL import ImageDraw, Image
 import sys
 import torch
 from tqdm import tqdm
+import yaml
 
 from EV_FlowNet.test import vis_flow
 from utils.common import mean
@@ -43,7 +44,7 @@ def join_images(images):
     return images
 
 
-def get_events_text(args, batch):
+def event_statistics(args, batch):
     element_index = batch['events']['element_index']
     num_prefix_events = (element_index < args.prefix_length).sum()
     sequence_length = batch['augmentation_params']['sequence_length'].item()
@@ -51,6 +52,12 @@ def get_events_text(args, batch):
     num_suffix_events = (element_index >= first_suffix_idx).sum()
     num_events = element_index.numel()
     num_prediction_events = num_events - num_prefix_events - num_suffix_events
+    return num_prefix_events, num_prediction_events, num_suffix_events
+
+
+def get_events_text(args, batch, statistics):
+    num_prefix_events, num_prediction_events, num_suffix_events = statistics
+    num_events = batch['events']['element_index'].numel()
     prefix_quantile = num_prefix_events * 100 / num_events
     suffix_quantile = num_suffix_events * 100 / num_events
     pred_quantile = num_prediction_events * 100 / num_events
@@ -60,13 +67,25 @@ def get_events_text(args, batch):
         f'{num_suffix_events} ({suffix_quantile:.2f}%)'
 
 
+def items2floats(array):
+    return list(float(x) for x in array)
+
+
 def prepare_text(args, batch, loss, parts, weights):
     parts = list(map(list, parts))
     loss_text = ' + '.join([f'{y}*{x:.4f}'
                             for x, y in zip(map(mean, parts), weights)])
-    return f'loss: {loss:.4f} = {loss_text}\n' + '\n'.join(map(
+    ev_stats = event_statistics(args, batch)
+    statistics = {'loss': float(loss), 'smoothness': items2floats(parts[0]),
+                  'photometric': items2floats(parts[1]),
+                  'border': items2floats(parts[2]),
+                  'prefix_size': int(ev_stats[0]),
+                  'pred_size': int(ev_stats[1]),
+                  'suffix_size': int(ev_stats[2])}
+    text = f'loss: {loss:.4f} = {loss_text}\n' + '\n'.join(map(
         array2text, parts, ['smoothness', 'photometric', 'border'])) + \
-        '\n' + get_events_text(args, batch)
+        '\n' + get_events_text(args, batch, ev_stats)
+    return text, statistics
 
 
 def put_image(dst, src, x0, y0):
@@ -112,12 +131,13 @@ def visualize(args, batch, loss, parts, weights, prediction):
     joined_images = join_images(batch['images'])
 
     res = np.zeros([80, joined_images.shape[1], 3], dtype=np.uint8)
-    text = prepare_text(args, batch, loss, parts, weights)
+    text, statistics = prepare_text(args, batch, loss, parts, weights)
     image = Image.fromarray(res)
     ImageDraw.Draw(image).text((0, 0), text, (255, 255, 255))
     image = np.asarray(image)
     flow_image = visualize_predictions(args, batch, prediction)
-    return np.concatenate([image, joined_images, flow_image], axis=0)
+    image = np.concatenate([image, joined_images, flow_image], axis=0)
+    return image, statistics
 
 
 def choose_output_path(args):
@@ -138,8 +158,19 @@ def image_writer(image_queue):
         data = image_queue.get()
         if data is None:
             break
-        path, image = data
-        imwrite(path, image)
+        path, image, statistics = data
+        image_file, yaml_file = files(path)
+        if not image_file.is_file():
+            imwrite(image_file, image)
+        if not yaml_file.is_file():
+            with yaml_file.open('w') as f:
+                yaml.dump(statistics, f)
+
+
+def files(filename):
+    dirname = filename.parent
+    name = filename.name
+    return dirname/(name + '.png'), dirname/(name + '.yml')
 
 
 def main():
@@ -157,16 +188,15 @@ def main():
         sequence_length=args.prefix_length + args.suffix_length + 1)
     with torch.no_grad():
         for i, batch in tqdm(enumerate(loader), total=len(loader)):
-            output_file_path = output_dir/f'{i:04d}.png'
-            if output_file_path.is_file():
+            output_file_path = output_dir/f'{i:04d}'
+            if all(x.is_file() for x in files(output_file_path)):
                 continue
             loss, parts, tags, prediction = process_minibatch(
                 model, batch, FakeTimer(), args.device, args.is_raw,
                 evaluator, args.loss_weights, return_prediction=True)
-            visualization = visualize(args, batch, loss, parts,
-                                      args.loss_weights, prediction)
-            image_queue.put((output_file_path, visualization))
-            # imwrite(output_file_path, visualization)
+            visualization, stat = visualize(args, batch, loss, parts,
+                                            args.loss_weights, prediction)
+            image_queue.put((output_file_path, visualization, stat))
     for _ in range(num_writers):
         image_queue.put(None)
     worker.close()
